34119ef280c14e5a9c0731067fe6ee93b69e75ca
Alisha <ialisha009@gmail.com>
2021-10-05 23:28:13 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Home',
+ });
+});
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Home',
+ });
+});
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About',
+ });
+});
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects',
+ });
+});
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services',
+ });
+});
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contactme', { title: 'Contact',
+ });
+});
+
+
+module.exports = router;
